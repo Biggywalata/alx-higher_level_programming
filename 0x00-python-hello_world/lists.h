a080694c044a12e15a795dfb506a2bd30875a790
Biggywalata <edwardinho6@gmail.com>
2023-01-02 17:09:46 -0800

    python commit

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,10 +1,7 @@
-#ifndef LISTS_H 
- #define LISTS_H 
-  
- #include <stdlib.h> 
-  
-  
- typedef struct listint_s 
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+typedef struct listint_s 
  { 
          int n; 
          struct listint_s *next; 
