c42b7ad83c7a62dd38625a2c79b9d1056caf4559
Biggywalata <edwardinho6@gmail.com>
2023-01-11 16:06:28 -0800

    my commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-    num = 0
-    den = 0
-    for score, weight in my_list:
-        num += score * weight
-        den += weight
-    return num / den
-) 
+        if len(my_list) == 0:
+                    return 0
+                return sum([mul(x[0], x[1]) for x in my_list]) / sum(x[1] for x in my_list)
+            def mul(x, y):
+                return x * y
+            ) 
